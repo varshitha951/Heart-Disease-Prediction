5ab7cf4ff5d16a0187deeb5fb6a305503935ff7d
varshitha <162705285+varshitha951@users.noreply.github.com>
2024-10-15 21:08:02 +0530

    Add files via upload

---
+++ HeartDiseasePrediction/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+# Load the model
+filename = 'heart-disease-prediction-knn-model.pkl'
+model= pickle.load(open(filename, 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('title.html')
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+@app.route('/heart')
+def heart():
+    return render_template('heart.html')
+
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+
+        age = int(request.form['age'])
+        sex = request.form.get('sex')
+        cp = request.form.get('cp')
+        trestbps = int(request.form['trestbps'])
+        chol = int(request.form['chol'])
+        fbs = request.form.get('fbs')
+        restecg = int(request.form['restecg'])
+        thalach = int(request.form['thalach'])
+        exang = request.form.get('exang')
+        oldpeak = float(request.form['oldpeak'])
+        slope = request.form.get('slope')
+        ca = int(request.form['ca'])
+        thal = request.form.get('thal')
+        
+        data = np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
+        my_prediction = model.predict(data)
+        
+        return render_template('result.html', prediction=my_prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
